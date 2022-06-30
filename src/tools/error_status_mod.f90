9cd3ce5f3aa1d3fe97650897b77dfaa05b132192
Ray Chew <ray.chew@fu-berlin.de>
2022-06-30 23:07:39 +0200

    Added linear algebra error code to error_status_mod

--- src/tools/error_status_mod.f90
+++ src/tools/error_status_mod.f90
@@ -3,6 +3,7 @@ module error_status
     integer, parameter ::   NETCDF_READ_ERR   = 1,      &
                             COMMAND_LINE_ERR  = 2,      &
                             ALLOCATION_ERR    = 3,      &
-                            NOT_ALLOCATED_ERR = 4    
+                            NOT_ALLOCATED_ERR = 4,      &    
+                            LINALG_ERR = 5
 
 end module error_status
